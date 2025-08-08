344a908081c627135a16ddb2f4a9d64d7b56691e
Fdkuba90 <picodkuba@hotmail.com>
2025-08-07 19:24:43 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import FileUploader from '../components/FileUploader';
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ padding: '2rem' }}>
       <h1>Probabilidad de Incumplimiento CNBV</h1>
       <FileUploader />
     </div>
