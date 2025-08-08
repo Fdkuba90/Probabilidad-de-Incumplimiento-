480fc2ed87cb42a84b2d294c33e0298476adc722
Fdkuba90 <picodkuba@hotmail.com>
2025-08-07 19:08:10 -0600

    Update FileUploader.jsx

--- components/FileUploader.jsx
+++ components/FileUploader.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
-export default function FileUploader({ onFileSelect }) {
+export default function ResultCard({ fileName, empresa, fecha, puntaje, pi }) {
   return (
-    <div style={{ marginBottom: '1rem' }}>
-      <label style={{ display: 'block', marginBottom: 8 }}>
-        Subir PDF del Buró Empresarial:
-      </label>
-      <input type="file" accept=".pdf" onChange={onFileSelect} />
+    <div style={{
+      border: '1px solid #ccc',
+      padding: 20,
+      borderRadius: 8,
+      backgroundColor: 'white',
+      marginTop: 20
+    }}>
+      <p><b>Empresa:</b> {empresa}</p>
+      <p><b>Fecha:</b> {fecha}</p>
+      <p><b>Archivo cargado:</b> {fileName}</p>
+      <p><b>Puntaje total:</b> {puntaje}</p>
+      <p><b>Probabilidad de Incumplimiento (PI):</b> {pi}</p>
     </div>
   );
 }
