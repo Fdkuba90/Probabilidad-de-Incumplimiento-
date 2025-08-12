c4ac5874e7a7965b9b1134854fd0cd9581c07b53
Fdkuba90 <picodkuba@hotmail.com>
2025-08-11 21:14:25 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  // por si Next intenta empacar dependencias nativas en serverless
+  experimental: {
+    serverComponentsExternalPackages: ["pdf-parse", "formidable"]
+  }
 };
-
 module.exports = nextConfig;
