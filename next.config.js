8736194c8f2e69a67f94972a6324fca24ce429fc
Fdkuba90 <picodkuba@hotmail.com>
2025-08-15 11:31:18 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    serverComponentsExternalPackages: [
-      "pdf-parse",
-      "formidable",
-      "@napi-rs/canvas",
-      "tesseract.js",
-      "pdfjs-dist"         // <-- añade esto
-    ]
-  }
+  reactStrictMode: true
 };
+
 module.exports = nextConfig;
